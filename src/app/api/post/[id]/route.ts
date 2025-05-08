477c3b3e85b3590bf4bcc48216e5f9e90a640146
shong2430 <134196918+shong2430@users.noreply.github.com>
2025-05-09 06:06:04 +0800

    typeincorrect

--- src/app/api/post/[id]/route.ts
+++ src/app/api/post/[id]/route.ts
@@ -1,9 +1,14 @@
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(_: NextRequest, { params }: { params: { id: string } }) {
+export async function GET(
+  req: NextRequest,
+  context: { params: { id: string } }
+) {
+  const { id } = context.params;
+
   const post = await prisma.post.findUnique({
-    where: { id: Number(params.id) },
+    where: { id: Number(id) },
   });
 
   if (!post) {
